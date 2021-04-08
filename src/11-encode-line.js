e0747d31625fc91f461325e2fac592517a0fa1de
Iryna Yakubenia <i.yakubenia@gmail.com>
2021-04-08 13:48:44 +0300

    feat: 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    const letter = str[i];
+    let count = 1;
+    let j = i + 1;
+
+    while (j < str.length && str[j] === letter) {
+      count += 1;
+      j += 1;
+    }
+
+    i += count - 1;
+    result += (count === 1) ? `${letter}` : `${count}${letter}`;
+  }
+  return result;
 }
 
 module.exports = encodeLine;
